2c0be8955c70ce5a7ebb4af7e7a11133dc80fdef
mycodesgit <lowexian@gmail.com>
2025-02-12 12:58:42 +0800

    Update

--- resources/views/studevalform/formevalsubjfac.blade.php
+++ resources/views/studevalform/formevalsubjfac.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Evaluate Faculty</h1>
+                    <h1>Evaluate Faculty</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
