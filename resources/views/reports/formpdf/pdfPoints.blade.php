62559e18b9c8bd5d3b06a531e8e688c367fb9822
mycodesgit <lowexian@gmail.com>
2025-03-04 10:09:27 +0800

    Update

--- resources/views/reports/formpdf/pdfPoints.blade.php
+++ resources/views/reports/formpdf/pdfPoints.blade.php
@@ -35,8 +35,8 @@
         $rate_period   = 'February 2024 - June 2024';   
         $sem_range     = '2nd Semester';                   
         $sch_yr        = request('schlyear');
-        $reviewer      = $fcs->fname . ' ' . substr($fcs->mname, 0, 1) . ' ' . $fcs->lname . ', ' . $fcs->rankcomma;
-        $reviewer_pos  =  $facDesignateId->designation . ', ' . $facDesignateId->college_name = str_replace(' Of ', ' of ', ucwords(strtolower($facDesignateId->college_name)));        ;
+        $reviewer      = $facDesignateId->fname . ' ' . substr($facDesignateId->mname, 0, 1) . ' ' . $facDesignateId->lname . ', ' . $facDesignateId->rankcomma;
+        $reviewer_pos  =  $facDesignateId->designation . ', ' . $facDesignateId->college_name = str_replace(' Of ', ' of ', ucwords(strtolower($facDesignateId->college_name)));
         $date_signed   = 'July 1, 2024';
         $endorser      = 'FERNANDO D. ABELLO, Ph. D.';
         $endorser_pos  = 'Endorser Position';
