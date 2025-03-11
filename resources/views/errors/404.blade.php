2e15a2a4140c33826904211650074b458c48bece
mycodesgit <lowexian@gmail.com>
2025-03-11 13:28:15 +0800

    Update

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>404 Page not found</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('template/dist/css/bootstrap.min.css') }}" crossorigin="anonymous">
+    <!-- Login Design -->
+    <link rel="stylesheet" href="{{ asset('template/dist/css/login-style.css') }}">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free-v6/css/all.min.css') }}">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="{{ asset('template/plugins/toastr/toastr.min.css') }}">
+    <!-- Logo -->
+    <link rel="shortcut icon" type="" href="{{ asset('template/img/CPSU_L.png') }}">
+
+    <style>
+        #notfound {
+            position: relative;
+        }
+
+        #notfound .notfound {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        .notfound {
+            max-width: 520px;
+            width: 100%;
+            line-height: 1.4;
+            text-align: center;
+        }
+
+        .notfound .notfound-404 {
+            position: relative;
+            height: 240px;
+        }
+
+        .notfound .notfound-404 h1 {
+            font-family: 'Montserrat', sans-serif;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            font-size: 200px;
+            font-weight: 900;
+            margin: 0px;
+            color: #262626;
+            text-transform: uppercase;
+            letter-spacing: -40px;
+            margin-left: -20px;
+        }
+
+        .notfound .notfound-404 h1>span {
+            text-shadow: -8px 0px 0px #fff;
+        }
+
+        .notfound .notfound-404 h3 {
+            font-family: 'Cabin', sans-serif;
+            position: relative;
+            font-size: 16px;
+            font-weight: 700;
+            text-transform: uppercase;
+            color: #262626;
+            margin: 0px;
+            letter-spacing: 3px;
+            padding-left: 6px;
+        }
+
+        .notfound h2 {
+            font-family: 'Cabin', sans-serif;
+            font-size: 20px;
+            font-weight: 400;
+            text-transform: uppercase;
+            color: #000;
+            margin-top: 0px;
+            margin-bottom: 25px;
+        }
+
+        @media only screen and (max-width: 767px) {
+            .notfound .notfound-404 {
+                height: 200px;
+            }
+
+            .notfound .notfound-404 h1 {
+                font-size: 200px;
+            }
+        }
+
+        @media only screen and (max-width: 480px) {
+            .notfound .notfound-404 {
+                height: 162px;
+            }
+
+            .notfound .notfound-404 h1 {
+                font-size: 162px;
+                height: 150px;
+                line-height: 162px;
+            }
+
+            .notfound h2 {
+                font-size: 16px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+                style="background: #04401f;">
+                {{-- <div id="particles-js"></div> --}}
+                <div class="featured-image mb-3">
+                    <center><img src="{{ asset('template/img/cpsulogov4.png') }}" class="img-fluid" id=""
+                            style="width: 100px; padding-top: 0px;"></center>
+                    <p class="text-white"
+                        style="font-family: 'Courier New', Courier, monospace; font-weight: 600; font-size: 1.5em !important">
+                        CPSU OFES</p>
+                </div>
+                <small class="text-white text-wrap text-center"
+                    style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Online Faculty Evaluation
+                    System</br></small>
+                {{-- <center><img src="{{ asset('template/img/cpsulogov4.png') }}" class="img-fluid" id="cpsulogoleftsideImage" style="width: 80%; padding-top: 0px;"></center> --}}
+            </div>
+
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4 text-center">
+                        <img src="{{ asset('template/img/cpsulogov4.png') }}" style="width:100px; margin-top: -250px"
+                            id="cpsulogoImage">
+                    </div>
+                    <div class="input-group mb-3 text-center">
+                        {{-- <img src="{{ asset('template/img/404error.png') }}" style="width:100%;" id=""> --}}
+                        <div id="">
+                            <div class="notfound">
+                                <div class="notfound-404">
+                                    <h3>Oops! Page not found</h3><br><br>
+                                    <h1><span>4</span><span>0</span><span>4</span></h1>
+                                </div>
+                                <h5>we are sorry, but the page you requested was not found</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <br><br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Moment -->
+    <script src="{{ asset('template/plugins/moment/moment.min.js') }}"></script>
+    <!-- Toastr -->
+    <script src="{{ asset('template/plugins/toastr/toastr.min.js') }}"></script>
+    {{-- <script src="{{ asset('particles/particles.js') }}"></script>
+    <script src="{{ asset('particles/app.js') }}"></script> --}}
+    <!-- Context -->
+    <script src="{{ asset('js/basic/contextmenu.js') }}"></script>
+
+</body>
+
+</html>
