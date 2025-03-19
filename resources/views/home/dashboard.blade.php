f5aa18607d40efa9c4eb59b3ddf2bd184e657058
mycodesgit <lowexian@gmail.com>
2025-03-19 14:36:17 +0800

    Update

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -81,7 +81,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12">
+                {{-- <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
                             <h5 class="card-title">
@@ -94,7 +94,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
